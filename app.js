cab7c37c1ab0f6bf3a57c87df2d35732d2a2febd
Mark Henwood <mcbhenwood@icloud.com>
2020-02-15 14:55:23 +0000

    Get merged PRs (for fixed repo) and show simple output

---
+++ app.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+const getPRData = async () => {
+    const resp = await axios.get('https://api.github.com/search/issues\?q\=is:pr+repo:uktrade/data-hub-frontend+author:mcbhenwood+is:merged+created:\>\=2019-09-01')
+    return resp.data
+}
+
+const formatPrLine = ({number, html_url, created_at, closed_at}) =>
+    `number ${number}, html_url, ${html_url}`
+
+const main = async () => {
+    const data = await getPRData()
+    data.items.map(pr => console.log(formatPrLine(pr)))
+}
+
+main()
